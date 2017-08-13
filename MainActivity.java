17d18f14c208af633d76eb56b59ea4167c26f2f4
Rachit Yadav <rachitnitsri@gmail.com>
2017-08-14 00:07:18 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,59 @@
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ *
+ * 
+ */
+package com.rachitnitsri.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int quantity = 0;
+    public void submitOrder(View view) {
+        String priceMessage = "$"+quantity*5;
+        displayMessage(priceMessage);
+    }
+    public void increment(View View){
+        quantity++;
+        display(quantity);
+    }
+    public void decrement(View View){
+        if(quantity>0)
+        quantity--;
+
+        display(quantity);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+}
